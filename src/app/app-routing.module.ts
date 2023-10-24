7694a1b084fa55527bf11ef15e9ee92e686ed829
ABINASH GOUDA <Abinash.Gouda01@futuregenerali.in>
2023-10-24 16:47:01 +0530

    basic dashboard completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BasicDashboardComponent } from './basic-dashboard/basic-dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'basicDashboard',component:BasicDashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
